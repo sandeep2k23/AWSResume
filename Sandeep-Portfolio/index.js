07ac72709f557860c091e17c7a831a99414b5647
sandeep23 <80738623+sandeep2k23@users.noreply.github.com>
2024-01-22 23:45:00 -0500

    Update and rename indes.js to index.js

--- Sandeep-Portfolio/index.js
+++ Sandeep-Portfolio/index.js
@@ -1,7 +1,7 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     let response = await fetch(
-        "https://wwjcx7tyxrbjmbkf3vc3teo3mu0qrvhq.lambda-url.ca-central-1.on.aws/"
+        "https://fyjri2io3gctsuavmfwp4pldn40rrddp.lambda-url.us-east-1.on.aws/"
     );
     let data = await response.json();
     counter.innerHTML = `👀 Views: ${data}`;
