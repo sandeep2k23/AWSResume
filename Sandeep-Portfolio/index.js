b746e3b2ab5e0eae8a8421f1eef17f8f6f54d260
sandeep23 <80738623+sandeep2k23@users.noreply.github.com>
2024-01-25 11:00:32 -0500

    Delete Sandeep-Portfolio/index.js

--- Sandeep-Portfolio/index.js
+++
@@ -1,9 +0,0 @@
-const counter = document.querySelector(".counter-number");
-async function updateCounter() {
-    let response = await fetch(
-        "https://fyjri2io3gctsuavmfwp4pldn40rrddp.lambda-url.us-east-1.on.aws/"
-    );
-    let data = await response.json();
-    counter.innerHTML = `👀 Views: ${data}`;
-}
-updateCounter();
